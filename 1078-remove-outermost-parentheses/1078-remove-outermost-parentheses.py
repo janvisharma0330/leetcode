5db90ec0a3fe56133509dbbe9d919033ba8b748e
Spongbob <107802332+janvisharma0330@users.noreply.github.com>
2024-07-14 16:06:13 +0530

    Time: 37 ms (68.35%), Space: 16.6 MB (39.29%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
@@ -1,10 +1,10 @@
 class Solution:
     def removeOuterParentheses(self, S):
-        res, opened = [], 0
+        res, stack = [], 0
         for c in S:
-            if c == '(' and opened > 0: res.append(c)
-            if c == ')' and opened > 1: res.append(c)
-            opened += 1 if c == '(' else -1
+            if c == '(' and stack > 0: res.append(c)
+            if c == ')' and stack > 1: res.append(c)
+            stack += 1 if c == '(' else -1
         return "".join(res)
 
         
